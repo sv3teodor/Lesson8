dac4430676fbf43ed46298ec0ed76f9565460aad
Fedor Kolpakov <sv3-teodor@yandex.ru>
2024-04-08 12:20:12 +0300

    Задание 8. Версия альфа 0.3

---
+++ api-tests/src/main/java/endpoints/AuthApi.java
@@ -0,0 +1,31 @@
+package endpoints;
+
+import dto.request.DTOUserProfile;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import org.aeonbits.owner.ConfigFactory;
+import utils.ApiProperites;
+import utils.RestApiBuilder;
+
+public class AuthApi {
+    static ApiProperites prop = ConfigFactory.create(ApiProperites.class);
+
+    AuthApi() {
+    }
+
+    @Step("Логин пользователя")
+    public static Response userLogin(DTOUserProfile DTOUserProfile) {
+        return new RestApiBuilder(prop.baseUrl()).build()
+                .body(DTOUserProfile)
+                .post(Urls.USER_LOGIN);
+    }
+
+    @Step("Регистрация пользователя")
+    public static Response userRegister(DTOUserProfile DTOUserProfile) {
+        return new RestApiBuilder(prop.baseUrl()).build()
+                .body(DTOUserProfile)
+                .post(Urls.USER_REGISTER);
+    }
+
+
+}
